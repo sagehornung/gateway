ae1bd0da09f858b9e9440317bccde56308a29cfb
sagehornung <sagehornung@gmail.com>
2018-04-17 20:53:45 -0700

    Initial application generated by JHipster-4.14.3

---
+++ src/main/java/com/artlabwest/app/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.artlabwest.app.config.audit;
---
+++ src/main/java/com/artlabwest/app/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.artlabwest.app.web.rest.vm;
